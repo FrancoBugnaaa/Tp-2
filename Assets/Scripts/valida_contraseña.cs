443213587bc3486929a566a9d3a0500f906edf06
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-04-02 14:27:04 -0300

    Tp Completed
    5 to 7

---
+++ Assets/Scripts/valida_contraseña.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class valida_contraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    public Text IngresoUsr;
+    string contraseñaUsr;
+    public GameObject cartelitoMsj;
+    public Text txtMensaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void validarContraseña() {
+        contraseñaUsr = IngresoUsr.text;
+        if (contraseñaUsr == contraseñaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            txtMensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            txtMensaje.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
